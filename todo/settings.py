bcbf1891863552f6d3c0f8db4207c1bd7a75baca
ahmedibrahimx25 <96432629+ahmedibrahimx25@users.noreply.github.com>
2026-09-07 20:38:21 +0300

    Serve static files with whitenoise in production

--- todo/settings.py
+++ todo/settings.py
@@ -50,6 +50,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",  
     'django.middleware.common.CommonMiddleware',
@@ -136,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
